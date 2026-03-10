57eae57ea3c9e159764cc343547768a27c82afc6
Soumyadip <onemystery.0o@gmail.com>
2026-03-10 14:41:43 +0530

    a

---
+++ react-users-app/src/components/Users.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
